84a0fe8627aaa32a88ced7f54bb2f7ece7d3d3f4
Michael89 <likvidator89@gmail.com>
2010-07-01 16:59:05 +0300

    second commit

--- views.py
+++ views.py
@@ -5,3 +5,5 @@ from scandinavian_auction.auction.models import Auction
 def main(request):
     auctions=Auction.objects.all()
     return render_to_response('home.html',{'auctions':auctions},context_instance=RequestContext(request));
+
+"""Some text"""
