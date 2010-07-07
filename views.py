415284fb7dbe34d59c7cb213662bd77b5582fa7c
Michael89 <likvidator89@gmail.com>
2010-07-07 19:10:35 +0800

    sdfasdf

--- views.py
+++ views.py
@@ -5,5 +5,3 @@ from scandinavian_auction.auction.models import Auction
 def main(request):
     auctions=Auction.objects.all()
     return render_to_response('home.html',{'auctions':auctions},context_instance=RequestContext(request));
-
-"""Some text"""
