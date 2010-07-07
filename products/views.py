259a62defc1eaff8a43f000752fe475ace804ed6
Denis Babak <debugger88@gmail.com>
2010-07-07 16:04:44 +0300

    /products page

--- products/views.py
+++ products/views.py
@@ -3,8 +3,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from scandinavian_auction.products.forms import ProductForm
 from scandinavian_auction.products.models import Product
 from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 def products_list(request):
-    products = Product.objects.get()
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 10)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+    return render_to_response('products.html',{'products':products},context_instance=RequestContext(request))
     
     
